eeb098b09856476681eba0e3e97eea46bbe1a9c2
AraceliGS <aragutarras@gmail.com>
2018-01-21 13:12:11 -0500

    añadiendo avances

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
   $('.modal').modal();
+  alert('Hola');
+  // Variables
+  var $addButton = $('#add-button');
+  var $telephoneInput = $('#icon_telephone');
+  // Comprobando el número de dígitos ingresados en el input del modal
+  $telephoneInput.on('input', function() {
+    console.log($('#icon_telephone').val());
+    if ($telephoneInput.val().trim().length === 9) {
+      //console.log($telephoneInput.val().trim().length);
+      $addButton.removeAttr('disabled');
+      $addButton.click(function() {
+        alert('¡Funciona!');
+      });
+    } else {
+      if ($telephoneInput.val().trim().length < 9 || $telephoneInput.val().trim().length > 9) {
+        $addButton.attr('disabled', true);
+        $telephoneInput.one('keyup', function() {
+          alert('Escriba un nùmero de 9 digítos');
+        });
+      }
+    }
+  });
 });
