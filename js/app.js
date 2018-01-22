5c60d6bb8594c49d252f4702e3f7d92ae8bc99ae
AraceliGS <aragutarras@gmail.com>
2018-01-22 11:16:36 -0500

    añadiendo avances en la funcionalidad del modal

--- js/app.js
+++ js/app.js
@@ -10,25 +10,30 @@ $(document).ready(function() {
     console.log($('#icon_prefix').val().trim().length);
     console.log($('#icon_telephone').val().trim().length);
     // Analizando el nombre escrito 
-    var $name = $('#icon_prefix').val().trim();
+    var $name = $('#icon_prefix').val();
     var PATTERNNAME =/[A-z]/g;
     var $result1 = $name.match(PATTERNNAME);
     // Analizando el nùmero escrito
-    var $number = $('#icon_telephone').val().trim();
+    var $number = $('#icon_telephone').val();
     var PATTERNPHONENUMBER = /[0-9]/g;
     var $result2 = $number.match(PATTERNPHONENUMBER);
-    if ($result1.length !== 0 && $result2.length === 9) {
+    if ($result1 !== ' ' && $result2.length === 9) {
       $addButton.removeAttr('disabled');
       $addButton.one('click', function() {
         alert('¡Funciona!');
       });
+    } else if ($result1 === ' ' && $result2.length === 9) {
+      $addButton.attr('disabled', true);
+    } else if($result1 !== ' ' && $result2.length < 9 || $result2.length > 9) {
+    $addButton.attr('disabled', true);
     } else {
-      if ($telephoneInput.val().trim().length < 9 && $nameInput.val().trim().length === 0 || $telephoneInput.val().trim().length > 9 && $nameInput.val().trim().length === 0) {
         $addButton.attr('disabled', true);
         /*$telephoneInput.one('keyup', function() {
           alert('Escriba un nùmero de 9 digítos');
         });*/
-      }
+    }
+    if ($result1 === '') {
+      $addButton.attr('disabled', true)
     }
   });
 });
